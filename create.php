2b20b409ebb0330250d3abb2457ab25179e952ff
Sontrinh68 <58957085+Sontrinh68@users.noreply.github.com>
2020-02-17 14:28:50 +0200

    Update create.php

--- create.php
+++ create.php
@@ -9,19 +9,23 @@
         </head>
     </html>
 <?php include 'db.php' ;
-$t= $_POST['time']; //method go with name of the variable 
+ //method go with name of the variable 
 $d= $_POST['date'];
+$t= $_POST['time'];
+$the_time = date('h:i A', strtotime($t));
 $n= $_POST['number'];
-$w= $_POST['wishes'];
-$na=$_POST['name'];
+$f=$_POST['fname'];
+$l=$_POST['lname'];
 $e=$_POST['email'];
-$sql= "insert into booking(time,date,number,wishes,name,email)
-values ('$t','$d','$n','$w','$na','$e')";
+
+
+$sql= "insert into booking(date,time,number,fname,lname,email)
+values ('$d','$t','$n','$f','$l','$e')";
 
 if($conn->query($sql)===TRUE){
     
-    echo'<p style="color:white;font-size:20px;font-family:cubic;margin:15px;">Booking successfully. </p>';
-    echo '<p style="color:white;font-size:20px;font-family:cubic;margin:15px;">Thank you! </p>';
+    echo'<p style="color:white;font-size:20px;font-family:cubic;margin:50px;">Booking successfully. </p>';
+    echo '<p style="color:white;font-size:20px;font-family:cubic;margin:50px;">Thank you! </p>';
 } else{
     echo "ERROR: " .$sql . "<br>" .$conn->error;
 
