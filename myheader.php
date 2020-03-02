011acfa201cb9d88829e3fcec5f5a668c5ec2378
Sontrinh68 <58957085+Sontrinh68@users.noreply.github.com>
2020-03-02 09:03:31 +0200

    Update myheader.php

--- myheader.php
+++ myheader.php
@@ -87,7 +87,31 @@
        top:12px;
        right:80px;
        }
-       
+      /* extra small devices ( phone, 600px and down) */
+  @media only screen and (max-width: 600px){
+    .order{
+      padding: 2px 10px;
+      top:15px;
+      right:200px;
+    }
+
+  }
+  /* small devices ( portrait tablets and large phones, 600px and up) */
+  @media only screen and (min-width: 600px){
+
+  }
+  /* medium devices ( landscape tablets, 768px and up) */
+  @media only screen and (min-width: 768px){
+
+  }
+  /* large devices ( laptop/desktop, 992px and up)*/
+  @media only screen and (min-width: 992px){
+
+  }
+  /* Extra large devices (large laptops and desktops, 1200px and up) */
+  @media only screen and (min-width: 1200px){
+
+  }   
     
       </style>
 
