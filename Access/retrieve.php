9b0a4a8312639d47edb5ed524161ea562a0867ae
DinhThuan206 <58892328+DinhThuan206@users.noreply.github.com>
2020-03-04 11:55:29 +0200

    change: include myheader_nobell.php

--- Access/retrieve.php
+++ Access/retrieve.php
@@ -1,6 +1,6 @@
 
 <?php include '..\db.php'?> 
-<?php include '..\myheader.php'?>   
+<?php include '..\myheader_nobell.php'?>   
 <!-- Header -->
 <!doctype html>
 <html lang="en">
@@ -113,8 +113,6 @@
 
   </head>
   <body>
-    
-    
 
       <h3>Order Information</h3>
 
@@ -185,7 +183,7 @@ $conn->close();
         color:#1DA1F2;
         font-size:30px;
         margin-bottom:5px;
-        margin-left:-27px;
+        margin-left:7px;
 
       }
       .instagram{
@@ -254,30 +252,13 @@ p{
         
     </div>
 
-  <button onclick="topFunction()" id="myBtn">
-		<i class="material-icons">change_history</i>
-    </button>
-
+ 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
-    <script>
-      mybutton = document.getElementById("myBtn");
-      window.onscroll = function() {scrollFunction()};
-      function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-      }
-    }
-    function topFunction() {
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
-    </script>
+    
   </body>
 </html>
